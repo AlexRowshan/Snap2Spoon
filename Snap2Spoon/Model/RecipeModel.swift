ae449300027b22839940da40e60e264c39217f9f
asanjaye <sanjaye@usc.edu>
2024-11-19 18:38:33 -0800

    Added constructor

--- Snap2Spoon/Model/RecipeModel.swift
+++ Snap2Spoon/Model/RecipeModel.swift
@@ -8,10 +8,22 @@
 import Foundation
 
 struct RecipeModel: Identifiable {
-    var id: ObjectIdentifier
+    var id: UUID
     let name:String
     let duration: String
     let difficulty: String
     let ingredients: [String]
     let instructions: [String]
+    
+    
+    init(name: String, duration: String, difficulty: String, ingredients: [String], instructions: [String]) {
+        self.id = UUID()  // Generates a unique identifier for each instance
+        self.name = name
+        self.duration = duration
+        self.difficulty = difficulty
+        self.ingredients = ingredients
+        self.instructions = instructions
+    }
 }
+
+
